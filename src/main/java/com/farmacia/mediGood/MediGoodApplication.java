cb0b249d0a080d9f32f5a39ef06f66714c85f831
Andres Briñez <brinezlopez08@gmail.com>
2024-06-20 14:09:32 -0500

    Archivos iniciales

---
+++ src/main/java/com/farmacia/mediGood/MediGoodApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.mediGood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/mediGood/MediGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.mediGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
