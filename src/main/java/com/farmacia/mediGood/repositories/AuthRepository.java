49d9f4653b3a7a06c16ba5f7816289c030828b74
Andres Briñez <brinezlopez08@gmail.com>
2024-06-29 22:42:56 -0500

    Se hace un cambio en el repositorio y se agrega un nuevo mmetodo

--- src/main/java/com/farmacia/mediGood/repositories/AuthRepository.java
+++ src/main/java/com/farmacia/mediGood/repositories/AuthRepository.java
@@ -3,5 +3,9 @@ package com.farmacia.mediGood.repositories;
 import com.farmacia.mediGood.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,String> {
+import java.util.Optional;
+
+public interface AuthRepository extends JpaRepository<User,String> {
+
+    Optional<User> findByPassword(String password);
 }
