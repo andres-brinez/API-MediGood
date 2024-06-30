d3fdb413eb097d1136349a60ae5306e00db135b3
Andres Briñez <brinezlopez08@gmail.com>
2024-06-29 23:09:45 -0500

    Se termina el servicio del login en cuanto a la validación de los datos ingresados por parte del usuario

--- src/main/java/com/farmacia/mediGood/services/AuthService.java
+++ src/main/java/com/farmacia/mediGood/services/AuthService.java
@@ -6,7 +6,9 @@ import com.farmacia.mediGood.models.entities.User;
 import com.farmacia.mediGood.repositories.AuthRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
@@ -15,14 +17,16 @@ public class AuthService {
 
     private  final AuthRepository authRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder encoder;
 
     //private final AuthenticationManager authenticationManager; // Se encarga de la autenticacion
 
-    public AuthService(AuthRepository authRepository, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthService(AuthRepository authRepository, JwtService jwtService, AuthenticationManager authenticationManager, PasswordEncoder encoder) {
 
         this.authRepository = authRepository;
         this.jwtService = jwtService;
         //this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
     }
 
     public User registerUser(UserRegisterDTO user){
@@ -51,7 +55,13 @@ public class AuthService {
         );*/
 
         var user = authRepository.findById(userInformation.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new BadCredentialsException("Invalid credentials"));
+
+        // chequear si las contrasenias son iguales
+        if (!encoder.matches(userInformation.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+
         // Generar el token
         var jwtToken = jwtService.generateToken(user);
 
