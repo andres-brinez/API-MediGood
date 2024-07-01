e8731d210e3170bec165d4ca24dbfe371e0f2d46
Andres Briñez <brinezlopez08@gmail.com>
2024-07-01 15:13:24 -0500

    Se crea los DTO para las respuestas de exíto y error

---
+++ src/main/java/com/farmacia/mediGood/models/DTOS/shared/ErrorResponseDTO.java
@@ -0,0 +1,13 @@
+package com.farmacia.mediGood.models.DTOS.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private String errorMessage;
+
+}
