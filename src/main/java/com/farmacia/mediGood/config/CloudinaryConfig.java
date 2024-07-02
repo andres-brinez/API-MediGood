d948c318e480ce05f89e404282edbc14fedb90e1
Andres Briñez <brinezlopez08@gmail.com>
2024-07-01 20:01:55 -0500

    Configuraciones necesarias para usar cloudinary

---
+++ src/main/java/com/farmacia/mediGood/config/CloudinaryConfig.java
@@ -0,0 +1,29 @@
+package com.farmacia.mediGood.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${CLOUDINARY_CLOUD-NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API-KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API-SECRET}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
+}
